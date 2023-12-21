d398b518f39f13bf61a92b6de7b502e47541669e
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2023-12-21 18:52:15 +0000

    Practicing pasting vectors in a file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import tensorflow as tf
 
+tf1 = tf.compat.v1
+tf1.disable_eager_execution()
+
+raw_data = [1., 2., 8., -1., 0., 5.5, 6., 13]
+spikes = tf1.Variable([False]* (len(raw_data)-1), name="spikes")
