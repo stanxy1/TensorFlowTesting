48a9eb207257dd3eabd4ae690a3f078de78d990b
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2024-02-06 13:19:12 +0000

    Pytorch integration

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
-import os
-import tensorflow as tf
-import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
 
-os.system("rm -rf logs")
-os.system("mkdir logs")
-tf1 = tf.compat.v1
-tf1.disable_eager_execution()
-sess = tf1.InteractiveSession()
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
-raw_data = np.random.normal(10, 1, 100)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
 
-alpha = tf.constant(0.05)
-curr_value = tf1.placeholder(tf.float32)
-prev_avg = tf1.Variable(0.)
-update_avg = alpha*curr_value + (1-alpha) * prev_avg
-avg_hist = tf1.summary.scalar("running_average", update_avg)
-value_hist = tf1.summary.scalar("incoming_values", curr_value)
-merged = tf1.summary.merge_all()
-writer = tf1.summary.FileWriter("./logs")
-init = tf1.global_variables_initializer()
+batch_size = 32
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+testloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
-with tf1.Session() as sess:
-    sess.run(init)
-    #sess._graph._add_op(sess.graph)
-    for i in range(len(raw_data)):
-        summary_str, curr_avg = sess.run([merged, update_avg],feed_dict={curr_value:raw_data[i]})
-        sess.run(tf1.assign(prev_avg, curr_avg))
-        print(raw_data[i], curr_avg)
-        writer.add_summary(summary_str, i)
+class CIFAR10Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.act1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.3)
+
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.flat = nn.Flatten()
+
+        self.fc3 = nn.Linear(8192, 512)
+        self.act3 = nn.ReLU()
+        self.drop3 = nn.Dropout(0.5)
+
+        self.fc4 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        # input 3x32x32, output 32x32x32
+        x = self.act1(self.conv1(x))
+        x = self.drop1(x)
+        # input 32x32x32, output 32x32x32
+        x = self.act2(self.conv2(x))
+        # input 32x32x32, output 32x16x16
+        x = self.pool2(x)
+        # input 32x16x16, output 8192
+        x = self.flat(x)
+        # input 8192, output 512
+        x = self.act3(self.fc3(x))
+        x = self.drop3(x)
+        # input 512, output 10
+        x = self.fc4(x)
+        return x
+
+model = CIFAR10Model()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+n_epochs = 20
+for epoch in range(n_epochs):
+    for inputs, labels in trainloader:
+        # forward, backward, and then weight update
+        y_pred = model(inputs)
+        loss = loss_fn(y_pred, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    acc = 0
+    count = 0
+    for inputs, labels in testloader:
+        y_pred = model(inputs)
+        acc += (torch.argmax(y_pred, 1) == labels).float().sum()
+        count += len(labels)
+    acc /= count
+    print("Epoch %d: model accuracy %.2f%%" % (epoch, acc*100))
+    torch.save(model.state_dict(), "cifar10model-epoch"+str(epoch)+".pth")
