3191355063ad7cfbdafa5699f1b8cac26279f144
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2024-02-09 01:19:04 +0000

    Everything is now organized! I will play around with kernel sizes and strides, then move on to next network!

---
+++ cifar_10_network/test_cifar10_conv.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import Compose, ToTensor, Resize
+from torch.utils.data import DataLoader
+
+transforms = Compose([ToTensor(), Resize((32, 32))])
+trainset = ImageFolder(root='test/', transform=transforms)
+batch_size = 3
+test = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+class CIFAR10Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.act1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.3)
+
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.flat = nn.Flatten()
+
+        self.fc3 = nn.Linear(8192, 512)
+        self.act3 = nn.ReLU()
+        self.drop3 = nn.Dropout(0.5)
+
+        self.fc4 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act1(x)
+        x = self.drop1(x)
+
+        x = self.conv2(x)
+        x = self.act2(x)
+        x = self.pool2(x)
+
+        x = self.flat(x)
+
+        x = self.act3(self.fc3(x))
+        x = self.drop3(x)
+
+        x = self.fc4(x)
+        return x
+
+model = CIFAR10Model()
+model.load_state_dict(torch.load("cifar10model-epoch6.pth"))
+classes = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+for inputs, labels in test:
+    y_pred = model(inputs)
+    for t in y_pred:
+        index = t.detach().numpy().argmax()
+        print(classes[index])
